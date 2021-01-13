76ed718174154454581249ca4cf45f79695fc950
ShinsukeYokote <sy114088@gmail.com>
2021-01-13 15:10:09 +0900

    userモデルアソシエーション、バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items
+  has_many :user_buys
+
+  validates :nick_name, presence: true
+  validates :email  , presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :encrypted_password, presence: true, length: {minimum: 6}, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }, confirmation: true
+  validates :last_name , presence: true
+  validates :first_name  , presence: true
+  validates :last_name_kana , presence: true
+  validates :first_name_kana , presence: true
+  validates :birthday  , presence: true
+         
 end
