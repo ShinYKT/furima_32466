0c45367b86352e20086b2d1456fe8eface1462f3
ShinsukeYokote <sy114088@gmail.com>
2021-01-13 15:36:25 +0900

    アプリケーションコントローラー編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   has_many :user_buys
 
   validates :nick_name, presence: true
-  validates :email  , presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :encrypted_password, presence: true, length: {minimum: 6}, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }, confirmation: true
+  validates :email  , uniqueness: true
+  validates :encrypted_password, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }, confirmation: true
   validates :last_name , presence: true
   validates :first_name  , presence: true
   validates :last_name_kana , presence: true
