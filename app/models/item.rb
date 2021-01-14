c4956b164f4fd4cc32c3493a9688b8de9f5ba852
ShinsukeYokote <sy114088@gmail.com>
2021-01-15 00:14:41 +0900

    activehash完成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,22 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one:user_buy
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery_day
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :state
+
 
   validates :image, presence: true
   validates :name, presence: true
   validates :text , presence: true
-  validates :category_id, presence: true
-  validates :state_id , presence: true
-  validates :delivery_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_day_id, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :state_id , numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :delivery_day_id, numericality: { other_than: 1 }
   with_options presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 } do
     validates :price
   end
