222ab745d79f26b09f9df96ee9f6e9522ea79c18
ShinsukeYokote <sy114088@gmail.com>
2021-01-19 21:32:59 +0900

    コード修正#4

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name { 'test' }
     text { 'test' }
-    category_id { '2' }
-    state_id  { '2' }
-    delivery_fee_id { '2' }
-    prefecture_id { '2' }
-    delivery_day_id { '2' }
-    price { '400' }
+    category_id { 2 }
+    state_id  { 2 }
+    delivery_fee_id { 2 }
+    prefecture_id { 2 }
+    delivery_day_id { 2 }
+    price { 400 }
     association :user
   end
 end
