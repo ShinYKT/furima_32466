65992a61ce49c8949a64ea1752e2741a71e15516
ShinsukeYokote <sy114088@gmail.com>
2021-01-20 20:40:17 +0900

    buysモデル、テーブル作成

---
+++ db/migrate/20210120113152_create_buys.rb
@@ -0,0 +1,13 @@
+class CreateBuys < ActiveRecord::Migration[6.0]
+  def change
+    create_table :buys do |t|
+      t.string :postal_code,  null: false
+      t.integer :prefecture_id, null:false
+      t.string :municipality, null:false
+      t.string :address, null:false
+      t.string :phone_number, null:false
+      t.references :user_buy, freign_key: true
+      t.timestamps
+    end
+  end
+end
