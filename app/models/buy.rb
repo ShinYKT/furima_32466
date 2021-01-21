a8fe4c4857f36e96f8fc57d488c1a1e315473476
ShinsukeYokote <sy114088@gmail.com>
2021-01-21 09:24:01 +0900

    ルーティング

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,3 +1,16 @@
 class Buy < ApplicationRecord
 　belongs_to :user_buy
+
+  with_options presence: true do
+    varidates :postal_code
+    varidates :prefectture_id
+    varidates :municipality
+    varidates :addres
+    varidates :building_name
+    varidates :phone_number
+  end
+
+
+
 end
+
--- app/models/user_buy.rb
+++ app/models/user_buy.rb
@@ -2,4 +2,6 @@ class UserBuy < ApplicationRecord
   belongs_to :user
   has_one :buy
   belongs_to :item
+
+  
 end
