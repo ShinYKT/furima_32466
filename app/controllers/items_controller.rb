765f05aad0461ed269c57005c00ffe1c25cdfab5
ShinsukeYokote <sy114088@gmail.com>
2021-01-22 14:46:35 +0900

    コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
 
 
   def edit
-    if @item.user_id != current_user.id
+    if @item.user_id != current_user.id or @item.user_buy.present?
       redirect_to root_path
     end
   end
